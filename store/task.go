d68eb680e32fe50b2b09758252f0d85937910d5a
kenwoo9y <kenwoo9y@gmail.com>
2024-03-21 21:12:38 +0900

    add AddTask method to store

--- store/task.go
+++ store/task.go
@@ -7,6 +7,28 @@ import (
 	"github.com/kenwoo9y/todo-api-go/entity"
 )
 
+func (r *Repository) AddTask(
+	ctx context.Context, db Execer, t *entity.Task,
+) error {
+	t.CreatedAt = r.Clocker.Now()
+	t.UpdatedAt = r.Clocker.Now()
+	sql := `INSERT INTO task
+		(title, description, status, created_at, updated_at) VALUES (?, ?, ?, ?, ?)`
+	result, err := db.ExecContext(
+		ctx, sql, t.Title, t.Status,
+		t.CreatedAt, t.UpdatedAt,
+	)
+	if err != nil {
+		return err
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+	t.ID = entity.TaskID(id)
+	return nil
+}
+
 func (r *Repository) ListTasks(
 	ctx context.Context, db *sqlx.DB,
 ) (entity.Tasks, error) {
