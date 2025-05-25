0717779b7a6d489a066124c5f77daa52e1b1b52b
kenwoo9y <kenwoo9y@gmail.com>
2025-05-25 16:31:31 +0900

    add entity

---
+++ api/internal/entity/task.go
@@ -0,0 +1,22 @@
+package entity
+
+import "time"
+
+type TaskStatus string
+
+const (
+	TaskStatusTodo  TaskStatus = "ToDo"
+	TaskStatusDoing TaskStatus = "Doing"
+	TaskStatusDone  TaskStatus = "Done"
+)
+
+type Task struct {
+	ID          int64      `json:"id"`
+	Title       string     `json:"title"`
+	Description string     `json:"description"`
+	DueDate     *time.Time `json:"due_date"`
+	Status      TaskStatus `json:"status"`
+	OwnerID     int64      `json:"owner_id"`
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
+}
---
+++ api/internal/entity/user.go
@@ -0,0 +1,13 @@
+package entity
+
+import "time"
+
+type User struct {
+	ID        int64     `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
